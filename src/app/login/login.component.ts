b5f3d584e4f1c92cae028db81d44fb2e82220cac
Shivani <shivani@techcompose.com>
2024-05-02 15:30:17 +0530

    set angular version and signals implementation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, computed, effect, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -10,7 +10,23 @@ import { CommonModule } from '@angular/common';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+        const firstName = signal('John');
+        const lastName = signal('Doe');
+        const age = signal(30);
+    
+        const fullName = computed(() => `${firstName()} ${lastName()}`);
+        const isAdult = computed(() => age() >= 18);
+
+        effect(() => {
+          console.log(`Full Name: ${fullName()}`);
+          console.log(`Is Adult: ${isAdult()}`);
+        });
+    
+        firstName.set('Jane');
+        lastName.set('Smith');
+        age.set(25);
+  }
 
   ngOnInit(): void {
   }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from '../login/login.component';
-
+import {
+  computed,
+  effect,
+  signal,
+} from '@angular/core';
 @Component({
   selector: 'app-product',
   standalone: true,
@@ -11,10 +15,40 @@ import { LoginComponent } from '../login/login.component';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    const productItems = signal([{ name: 'Product H', price: 1 }])
+    effect(() => {
+      console.log('productItems:', `${productItems()[1]['name']}`)
+    })
+    productItems.mutate((ele: any) => {
+      ele.push( { name: 'Product S', price: 0 })
+    })
+   }
+
+  items = [{ name: 'Product A', price: 10 }, { name: 'Product B', price: 15 }, { name: 'Product C', price: 20 }];
+  
+  itemList = signal(this.items);
+  
+  totalPrice = computed(() => {
+    return this.itemList().map((ele) => ele?.price - 5);
+  });
 
   ngOnInit(): void {
     console.log('hey from product')
+    const x = signal(5);
+    const y = signal(3);
+    const z = computed(() => x() + y());
+    console.log(z()); // 8
+
+    x.set(10);
+    console.log(z()); // 13
   }
+  
+  public removeItem(item: any) {
+    // this.itemList.set(this.itemList().filter((ele) => ele?.price === 10))
+    // Update the itemList signal by removing the selected item
+    this.itemList.set(this.itemList().filter((i) => i !== item));
+  }
+
 
 }
